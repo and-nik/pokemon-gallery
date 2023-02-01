726daab4988c6c40cf5b03e4c4cc3675f5eaa79c
Andrei <iamandry@icloud.com>
2023-02-01 21:13:55 +0300

    Application settings were added

---
+++ PokemonApplication/Sources/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Pokemon Application
+//
+//  Created by And Nik on 24.01.23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+    
+}
+
