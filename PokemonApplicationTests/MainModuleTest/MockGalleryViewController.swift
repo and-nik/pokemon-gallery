23f7ae69f00db149c0f89255eebeab25b2df824e
Andrei <iamandry@icloud.com>
2023-02-01 21:28:45 +0300

    PokemonApplicationTests were added

---
+++ PokemonApplicationTests/MainModuleTest/MockGalleryViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MockGalleryViewController.swift
+//  PokemonApplicationTests
+//
+//  Created by And Nik on 01.02.23.
+//
+
+import Foundation
+@testable import PokemonApplication
+
+final class MockGalleryViewController: GalleryViewControllerProtocol{
+    func failureInternetConnection() {
+        print("failureInternetConnection")
+    }
+    
+    func successInternetConnection() {
+        print("successInternetConnection")
+    }
+    
+    func presentPokemonInfoVC(VC: PokemonApplication.PokemonInfoViewController) {
+        print("presentPokemonInfoVC")
+    }
+    
+    func searchResult(pokemonsUrls: [PokemonApplication.PokemonUrl]) {
+        print("searchResult")
+    }
+    
+}
